cf7ab1022f91b27dff2cb833b8a823d6ca7bdbcf
dino buljubasic <dbnex14@gmail.com>
2020-01-14 10:11:15 -0800

    minor changes

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,21 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "1@b.com", body: "Hello"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // trigger UITableViewDataSource delegate methods
+        tableView.dataSource = self
+//        tableView.delegate = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        //register custom cell MessabeCell with table view
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -35,3 +46,29 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+// addopting UITableViewDataSource means that when our tableView
+// loads up, it will make request for data. This is protocol
+// responsible for populating table view
+//MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // which cell to display in each row of our table view
+        // so we have to create cell and return it to table view
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body // this is main part of info in cell
+        return cell
+    }
+}
+
+// This delegate is responsible for user interaction with table view but in chat app, we dont want that, so we commented out
+//MARK: - UITableViewDelegate
+//extension ChatViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row) // tap will print indexPath
+//    }
+//}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Dino B on 2020-01-13.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        // this is called when we create new message cell from MessageCell.xib
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
